7b3846b2a110888805ebf7b9ff1f28d20c87e8ef
Nebil Muhidin <nebilmuhidin57@gmail.com>
2025-02-27 21:47:04 +0300

    finalized the functionality

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-    
+from fastapi import FastAPI
+from routers import auth_router, post_router
+import uvicorn
+
+app = FastAPI()
+
+app.include_router(auth_router.router)
+app.include_router(post_router.router)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
